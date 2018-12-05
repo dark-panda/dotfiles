0313b2e505cbb9d695a86d4323f491bb7281e98e
J Smith <dark.panda@gmail.com>
2018-12-05 11:08:30 -0400

    Make fzf case insensitive.

--- zsh/jaysh/include.d/25_fzf.zsh
+++ zsh/jaysh/include.d/25_fzf.zsh
@@ -13,7 +13,7 @@
 { which fzf >& /dev/null } || return
 
 FZF_COMPLETION_TRIGGER='//'
-FZF_DEFAULT_OPTS='--height 80%'
+FZF_DEFAULT_OPTS='--height 80% -i'
 FZF_CTRL_T_OPTS='--preview ''[[ $(file --mime {}) =~ binary ]] &&
  echo {} is a binary file ||
  (highlight -O ansi -l {} ||
